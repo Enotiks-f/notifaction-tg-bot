65613f39afd4b2e6fef52100926805dfdb0c1aad
Titov Ilya <titov1608@gmail.com>
2026-03-24 14:58:04 +0300

    Создал модуль NotifacationTask и добавил lombok

--- src/main/java/pro/sky/telegrambot/entity/NotifacationTask.java
+++ src/main/java/pro/sky/telegrambot/entity/NotifacationTask.java
@@ -1,4 +1,28 @@
 package pro.sky.telegrambot.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@ToString
 public class NotifacationTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String message;
+    private LocalDateTime date;
+    private String status;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+    private Integer retry_count;
+
+
+
+
 }
